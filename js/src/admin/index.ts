92be2a42b13f0c082d2e5c575d68e5c8351bda07
IanM <ian@morland.me>
2025-11-06 11:30:27 +0000

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -2,5 +2,5 @@ import app from 'flarum/admin/app';
 import SettingsPage from './components/SettingsPage';
 
 app.initializers.add('fof/default-user-preferences', () => {
-  app.extensionData.for('fof-default-user-preferences').registerPage(SettingsPage);
+  app.registry.for('fof-default-user-preferences').registerPage(SettingsPage);
 });
